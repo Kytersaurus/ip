edbf5356029fb5ea409b90d638f28b618d20b906
Kyle Lee Yao Yu <kyleleeyaoyu@gmail.com>
2026-08-17 21:25:29 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Bob.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        String banner = " ____     ___    ____  \n"
+                + "| |_) )  / _ \\  | |_) ) \n"
+                + "|  _ \\  | | | | |  _ \\ \n"
+                + "| |_) | | |_| | | |_) |\n"
+                + "|____/   \\___/  |____/ \n";
+
+        String intro = "Hello! I'm B.O.B. (Best OpenAI Bot)\n";
+        String start = "What can I do for you?\n";
+        String exit = "Bye. Hope to see you again soon!\n";
+        String divider = "____________________________________________________________\n";
+        System.out.println(divider + banner + intro + start + divider + exit + divider);
+
+    }
+}
