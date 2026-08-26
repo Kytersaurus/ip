b91d5f1788fae1781cba4353bd3272e1491e3c7c
Kyle Lee Yao Yu <kyleleeyaoyu@gmail.com>
2026-08-26 14:40:52 +0800

    Level-3

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,4 +1,3 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Bob {
@@ -14,7 +13,7 @@ public class Bob {
         String exit = "Bye. Hope to see you again soon!\n";
         String divider = "____________________________________________________________\n";
         System.out.println(divider + banner + intro + start + divider);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
         try (Scanner in = new Scanner(System.in)) {
             while (true) {
@@ -24,16 +23,35 @@ public class Bob {
                     break;
                 }
                 else if (input.trim().equalsIgnoreCase("list")) {
-                    System.out.println(divider);
+                    System.out.println(divider + "Here are the tasks in your list:");
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println((i + 1) + ". " + tasks[i]);
+                        System.out.println((i + 1) + "." + "[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
                     }
                     System.out.println(divider);
                 }
+                else if (input.trim().startsWith("mark ")) {
+                    int index = Integer.parseInt(input.trim().substring(5)) -1;
+                    if (index >= taskCount) {
+                        System.out.println(divider + "No task at specified index " + (index + 1) + " found\n" + divider);
+                        continue;
+                    }
+                    tasks[index].markAsDone();
+                    System.out.println(divider + "Ok, I've marked this task as done:\n" + "[" + tasks[index].getStatusIcon() + "] " + tasks[index].getDescription() + "\n" + divider);
+                }
+                else if (input.trim().startsWith("unmark ")) {
+                    int index = Integer.parseInt(input.trim().substring(7)) -1;
+                    if (index >= taskCount) {
+                        System.out.println(divider + "No task at specified index " + (index + 1) + " found\n" + divider);
+                        continue;
+                    }
+                    tasks[index].markAsNotDone();
+                    System.out.println(divider + "Ok, I've marked this task as not done:\n" + "[" + tasks[index].getStatusIcon() + "] " + tasks[index].getDescription() + "\n" + divider);
+                }
                 else {
                     System.out.println(divider + "added: " + input.trim() + '\n' + divider);
-                    tasks[taskCount] = input.trim();
+                    Task newTask = new Task(input.trim());
                     taskCount++;
+                    tasks[taskCount-1] = newTask;
                 }
             }
         }
